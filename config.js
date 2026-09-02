0f0a4ff579f457bf36b54ccb26eda90569e20854
yggdgtsv84-wq <yggdgtsv84@privaterelay.appleid.com>
2026-09-03 09:33:51 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// Replace these two values with your Supabase Project URL and public anon/publishable key.
-// NEVER put the Supabase service_role/secret key here.
 window.SUPABASE_URL=https://phjfrcjrrivkbovgmzvd.supabase.co/rest/v1/;
 window.SUPABASE_ANON_KEY=sb_publishable_CzTuJxQPGo47iF62acUYag_pNG9YbhY;
