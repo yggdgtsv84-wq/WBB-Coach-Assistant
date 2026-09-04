94ef159e28cdcc23079b1e0530d3880b3026b40a
yggdgtsv84-wq <yggdgtsv84@privaterelay.appleid.com>
2026-09-04 10:14:50 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="wbcoach-v2";const A=["./","./index.html","./style.css","./config.js","./app.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
