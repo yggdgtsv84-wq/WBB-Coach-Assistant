2478df536fb40c68596a99c94610dddd20423566
yggdgtsv84-wq <yggdgtsv84@privaterelay.appleid.com>
2026-09-05 12:48:16 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Service worker intentionally disabled for the current web version.
-// This prevents an old cached build from interfering with the app.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.registration.unregister()));
